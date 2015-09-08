bdce9fb5a2a2b553d7b1b5fa017d9f50a97f06be
Dominic Cerquetti <dom@magfest.org>
2015-09-08 08:24:54 -0400

    add python API test

---
+++ python/python-api-ssl.py
@@ -0,0 +1,25 @@
+from pprint import pprint
+from rpctools.jsonrpc import ServerProxy
+
+# the URL you connect to
+your_API_URL = "http://whatever.com:777/jsonrpc/"
+
+# the client certificate .crt provided to you by an administrator
+your_client_cert_crt = "../selfsigned-X-client.crt"
+
+# the client certificate .key provided to you by an administrator
+your_client_cert_key = "../selfsigned-X-client.key"
+
+service = ServerProxy(
+    uri=your_API_URL,
+    cert_file=your_client_cert_crt,
+    key_file=your_client_cert_key
+)
+
+# now you can actually call the API functions
+pprint(service.attendee.lookup(badge_num=1))
+pprint(service.attendee.search('Test'))
+
+# if the rams instance has barcode support, you can look up a barcode
+your_barcode = "XXXXXX" # 6-digit string
+pprint(service.barcode.lookup_attendee_from_barcode(barcode_value=your_barcode))
