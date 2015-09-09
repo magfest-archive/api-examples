82efbcca0aa895d3e7444eeccfdc8403a746e94a
root <dom@magfest.org>
2015-09-09 11:21:39 -0400

    add working example

--- php/main.php
+++ php/main.php
@@ -2,7 +2,15 @@
 $cert = "../client.crt";
 $keyfile = "../client.key";
 $url = "https://stage.uber.magfest.org/jsonrpc/";
+
+$method = "attendee.lookup";
+$badge_number = 1;
+
+$post = '{"jsonrpc": "2.0", "method": "'.$method.'", "params": ['.$badge_number.']}';
 $ch = curl_init(); 
+curl_setopt( $ch, CURLOPT_POST,             1 );
+curl_setopt( $ch, CURLOPT_POSTFIELDS,       $post );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER,   true );
 curl_setopt($ch, CURLOPT_URL, $url); 
 curl_setopt($ch, CURLOPT_VERBOSE, 1); 
 curl_setopt($ch, CURLOPT_SSLCERT, $cert); 
@@ -11,4 +19,16 @@ curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_SSLCERTTYPE,"PEM");
 curl_setopt($ch, CURLOPT_SSLKEYTYPE,"PEM");
 $ret = curl_exec($ch);
+
+if( $ret === false )
+{
+    die( 'curl_exec() returned false .. message: ' . curl_error( $ch ) );
+}
+
+$http_status = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+curl_close( $ch );
+echo( $ret );
+echo '<br /><br />';
+echo "HTTP Return Code: {$http_status}";
+echo '<br />';
 ?>
