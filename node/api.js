811334c5640ee215ce36b851fe6f79232712c8ec
Dominic Cerquetti <binary1230@gmail.com>
2016-02-04 11:09:14 -0500

    fix node example

--- node/api.js
+++ node/api.js
@@ -1,29 +1,12 @@
-var rpc = require('node-json-rpc');
+const https = require('https');
+var fs = require("fs");
 
+var postData = JSON.stringify({jsonrpc: "2.0", method: "attendee.lookup", params: [5], id:(new Date).getTime()});
 var options = {
-  // int port of rpc server, default 5080 for http or 5433 for https
+  hostname: 'staging2.uber.magfest.org',
   port: 4444,
-  // string domain name or ip of rpc server, default '127.0.0.1'
-  host: 'staging2.uber.magfest.org',
-  // string with default path, default '/'
-  path: '/jsonrpc',
-  // boolean false to turn rpc checks off, default true
-  strict: true
-};
-
-// Create a server object with options
-var client = new rpc.Client(options);
-
-client.call(
-  {"jsonrpc": "2.0", "method": "attendee.lookup", "params": [5], "id": 0},
-  // {"jsonrpc": "2.0", "method": "myMethod", "params": [1,2], "id": 0},
-  function (err, res) {
-    // Did it all work ?
-    if (err) { console.log(err); }
-    else { console.log(res); }
-  }
-);
-
+  path: '/jsonrpc/',
+  method: 'POST',
   key: fs.readFileSync('../magdev-client.key'),
   cert: fs.readFileSync('../magdev-client.crt'),
   headers: { 'Content-Length': postData.length, 'Content-Type': "application/json" }
