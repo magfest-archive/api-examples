f105a4f060d3a465265c531d3a737a2b078afed4
Dominic Cerquetti <binary1230@gmail.com>
2016-02-04 11:17:29 -0500

    WIP using external lib, doesn't work

--- node/api.js
+++ node/api.js
@@ -1,34 +1,24 @@
-const https = require('https');
-var fs = require("fs");
-
-var postData = JSON.stringify({jsonrpc: "2.0", method: "attendee.lookup", params: [5], id:(new Date).getTime()});
-var options = {
-  hostname: 'staging2.uber.magfest.org',
-  port: 4444,
-  path: '/jsonrpc/API',
-  method: 'POST',
+/* TODO DOESNT WORK, need to get this info in there
   key: fs.readFileSync('../magdev-client.key'),
-  cert: fs.readFileSync('../magdev-client.crt'),
-  headers: { 'Content-Length': postData.length, 'Content-Type': "application/json" }
+  cert: fs.readFileSync('../magdev-client.crt'),*/
+
+var rpc = require('node-json-rpc');
+
+var options = {
+  port: 4444,
+  host: 'staging2.uber.magfest.org',
+  path: '/jsonrpc',
+  strict: true
 };
 
+// Create a server object with options
+var client = new rpc.Client(options);
 
-var req = https.request(options, function (res) {
-  console.log("STATUS: " + res.statusCode);
-  console.log("HEADERS: " + JSON.stringify(res.headers));
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log("BODY: " + chunk);
-  });
-  res.on('end', function () {
-    console.log('No more data in response.')
-  })
-});
-
-req.on('error', function (e) {
-  console.log("problem with request: " + e.message);
-});
-
-req.write(postData);
-req.end();
-
+client.call(
+  {"jsonrpc": "2.0", "method": "attendee.lookup", "params": [5], "id": 0},
+  function (err, res) {
+    // Did it all work ?
+    if (err) { console.log(err); }
+    else { console.log(res); }
+  }
+);
